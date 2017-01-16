48b99b2a5eee505979fb209e483ca35b2c7070fc
AnnaCarolinaLB <annaclb2008@hotmail.com>
2017-01-16 13:36:18 -0200

    Create index.php

---
+++ questao4/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <title>Gerenciador de tarefas 1.0</title>
+    </head>
+    <frameset rows="80,*" border="0">
+        <frame src="topo.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topo" />
+        <frame src="corpo.php" name="mainFrame" id="mainFrame" title="corpo" />
+    </frameset>
+</html>
